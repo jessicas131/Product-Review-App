40b11ae88859e7e485b52c4ecb36f38cc67f11eb
jstevens <jessica.stevens131@gmail.com>
2020-09-03 13:24:05 -0500

    fixing review

--- routes/products.js
+++ routes/products.js
@@ -7,18 +7,26 @@ const productsCtrl = require('../controllers/products');
 //   res.render('products/index', {title: 'Product App', user: req.user});
 // });
 
-router.get('/', isLoggedIn, productsCtrl.index);
+router.get('/', productsCtrl.index);
 
 router.post('/', isLoggedIn, productsCtrl.create);
 router.get('/new', isLoggedIn, productsCtrl.new);
 
-router.get('/:id', productsCtrl.show);
+router.get('/:id',isLoggedIn, productsCtrl.show);
 router.get('/:id/edit',isLoggedIn, productsCtrl.edit);
-router.put('/:id', productsCtrl.update);
+router.put('/:id',isLoggedIn, productsCtrl.update);
+
 
 function isLoggedIn(req, res, next) {
-  if ( req.isAuthenticated() ) return next();
-  res.redirect('/auth/google');
+  if (req.isAuthenticated()) {
+      next();
+  } else {
+      res.redirect('/auth/google')
+  }
 }
+// function isLoggedIn(req, res, next) {
+//   if ( req.isAuthenticated() ) return next();
+//   res.redirect('/auth/google');
+// }
 
 module.exports = router;
